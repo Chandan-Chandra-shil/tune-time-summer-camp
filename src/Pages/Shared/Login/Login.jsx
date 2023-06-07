2ee0df3249aaa334d2acd14e28b546332e4405c3
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-06-07 12:08:41 +0600

    add login page

--- src/Pages/Shared/Login/Login.jsx
+++ src/Pages/Shared/Login/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
-
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
   return (
-    <div>
-      <p>login page</p>
+    <div className="mx-auto container  md:my-44 rounded  ">
+      <form className="card-body md:w-1/2 mx-auto border rounded p-10 shadow">
+        <h2 className="text-3xl font-bold">Please Login</h2>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-lg">Email</span>
+          </label>
+          <input
+            type="text"
+            placeholder="email"
+            className="input input-bordered  "
+          />
+        </div>
+        <div className="form-control">
+          <label className="label ">
+            <span className="label-text text-lg">Password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="password"
+            className="input input-bordered"
+          />
+         
+        </div>
+        <div className="form-control mt-6">
+          <input
+            type="submit"
+            className="border px-8 py-2 bg-orange-200 rounded font-semibold hover:bg-orange-400"
+            value="Login"
+          />
+        </div>
+        <div className="divider">or</div>
+        <div className=" btn btn-outline hover:bg-orange-400 flex gap-2 justify-center items-center">
+          <span className="text-md">Login With</span>
+          <FcGoogle className="w-6 h-6"></FcGoogle>
+        </div>
+      </form>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import router from "./routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="font-[poppins] overflow-hidden">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
