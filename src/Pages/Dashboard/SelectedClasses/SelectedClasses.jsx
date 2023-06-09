ea96021cad77dc8a6c7ddc000deba297f5b6e631
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-06-09 18:15:41 +0600

    create useSelectedAllClasses hook and show data selected classes page

--- src/Pages/Dashboard/SelectedClasses/SelectedClasses.jsx
+++ src/Pages/Dashboard/SelectedClasses/SelectedClasses.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import UseSelectedAllClasses from '../../../hook/UseSelectedAllClasses';
 
 const SelectedClasses = () => {
+  const [selectedItem] = UseSelectedAllClasses()
   return (
     <div>
       <h2>select page</h2>
+      <p>{ selectedItem?.length}</p>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,20 @@ import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Provider/AuthProvider";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="font-[poppins] overflow-hidden">
       <HelmetProvider>
         <Toaster />
         <AuthProvider>
-          <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
         </AuthProvider>
       </HelmetProvider>
     </div>
